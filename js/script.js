9e239bf5fee0983a36f2c6d927642c7cf0027b14
hazypaint <hazypaint@hotmail.com>
2015-08-27 12:52:52 +0200

    feat: added the AJAX request for Wikipedia
    Used JSONP instead of CORS since Wikipedia doesn't support CORS

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ function loadData() {
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
     // creates link for AJAX request
-    var requestNYT = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + cityValue + "&api-key=bec2df7e772a904f941747a02d2bc3e7:4:72782796"
+    var requestNYT = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + cityValue + "&api-key=bec2df7e772a904f941747a02d2bc3e7:4:72782796";
     // console.log(requestNYT);
 
     // NYTimes AJAX request
@@ -33,7 +33,7 @@ function loadData() {
 
             //runs through each article and provides the requested data from each
             for (var i = 0 ; i < articles.length ; i++ ) {
-                var headlineAndLink = "<a href='" + articles[i].web_url + "'>" + articles[i].headline.main;
+                var headlineAndLink = "<a href='" + articles[i].web_url + "'target='_blank''>" + articles[i].headline.main;
                 var snippet = "<p>" + articles[i].snippet + "</p>" ;
                 $nytElem.append("<li class='article'>" + headlineAndLink  + "</a>" + snippet + "</li>" );
                 };
@@ -41,7 +41,32 @@ function loadData() {
 
         .fail ( function (e) {
             $nytHeaderElem.text("New York Times article could not be loaded.");
-        })
+        });
+
+    // wiki link created with Wikipedia API sandbox
+    var requestWiki = 'http://en.wikipedia.org/w/api.php?action=opensearch&search='+ cityValue + '&format=json&callback=wikiCallback'
+    // console.log("wikilink: " + requestWiki);
+
+    // Wikipedia AJAX request
+    $.ajax({
+        type: "GET",
+        url: requestWiki,
+        dataType: "jsonp",
+        success: function (response) {
+            var wikiArticles = response[1];
+
+            for (var i = 0 ; i < wikiArticles.length ; i++ ) {
+                var articleStr = wikiArticles[i];
+                console.log(articleStr);
+                var url = "http://en.wikipedia.org/wiki/" + wikiArticles[i];
+                $wikiElem.append("<li><a href='" + url + "'target='_blank'>" + articleStr + "</a></li>" );
+                };
+        },
+        error: function (errorMessage) {
+            $wikiElem.text("Wikipedia articles could not be loaded.");
+        }
+
+    });
 
     return false;
 };
